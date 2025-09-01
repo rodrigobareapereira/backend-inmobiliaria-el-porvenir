296458f6d6e1fe4d8840ba4c72a24e16bbc9f641
rodrigobareapereira <chichico175@gmail.com>
2025-09-01 14:49:06 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,12 @@
 // config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337), // Render inyecta su propio puerto
+  port: env.int('PORT', 1337),
   url: env('APP_URL', 'https://backend-inmobiliaria-el-porvenir.onrender.com'),
+
+  // 👇 ESTA PARTE ES OBLIGATORIA
+  app: {
+    // Lee APP_KEYS desde el entorno como array
+    keys: env.array('APP_KEYS'),
+  },
 });
