d4c7aec5bebd2d5f6de5e7e696fe58f06ab3d15f
rodrigobareapereira <chichico175@gmail.com>
2025-09-01 15:26:49 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
+  url: env('PUBLIC_URL', 'https://backend-inmobiliaria-el-porvenir.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
