492032a0e0f5cfd3aae8c87c25df8e353f0085fc
rodrigobareapereira <chichico175@gmail.com>
2025-09-01 15:19:54 -0400

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,10 @@
-// config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('APP_URL', 'https://backend-inmobiliaria-el-porvenir.onrender.com'),
-
-  // 👇 ESTA PARTE ES OBLIGATORIA
   app: {
-    // Lee APP_KEYS desde el entorno como array
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
